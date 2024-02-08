4ab09bd39a1fc86eb7e74b009a8c9d393fed92ea
Oksana Dudnik <123472443+asyadudnik@users.noreply.github.com>
2024-02-08 10:41:27 +0200

    commit package

---
+++ src/main/java/org/tests/Main.java
@@ -0,0 +1,13 @@
+package org.tests;
+
+import static org.tests.prime.PrimeNumberCheck.isPrime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(isPrime(19)); // true
+        System.out.println(isPrime(49)); // false
+
+    }
+}
