f3e9fa02a963ce20459baf6334330009b23d51cc
Oksana Dudnik <123472443+asyadudnik@users.noreply.github.com>
2024-02-08 10:46:42 +0200

    commit package

--- src/main/java/org/tests/Main.java
+++ src/main/java/org/tests/Main.java
@@ -1,13 +1,17 @@
 package org.tests;
 
+import static org.tests.fibonacci.PrintFibonacci.printFibonacciSequence;
 import static org.tests.prime.PrimeNumberCheck.isPrime;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //prime
         System.out.println(isPrime(19)); // true
         System.out.println(isPrime(49)); // false
+        //fibonacci
+        printFibonacciSequence(10);
 
     }
 }
---
+++ src/main/java/org/tests/fibonacci/PrintFibonacci.java
@@ -0,0 +1,22 @@
+package org.tests.fibonacci;
+
+public class PrintFibonacci {
+    private PrintFibonacci() {
+    }
+
+    public static void printFibonacciSequence(int count) {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.print(a + ", ");
+
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+
+
+}
